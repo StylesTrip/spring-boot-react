725050b8c27ba109a2e577886f0c790c5d553960
Ryan T <ryan.turinsky@gmail.com>
2020-03-24 22:16:41 -0400

    Initial commit

---
+++ src/main/java/com/ryan/reactspringboot/ReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.reactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootApplication.class, args);
+	}
+
+}
