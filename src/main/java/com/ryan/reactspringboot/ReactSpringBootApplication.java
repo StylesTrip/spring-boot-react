ef78b587a77bf3910f70ebd6ab5686fb330b7b94
Ryan T <ryan.turinsky@gmail.com>
2020-04-13 23:27:10 -0400

    add-jpa Adding in first domain class

--- src/main/java/com/ryan/reactspringboot/ReactSpringBootApplication.java
+++ src/main/java/com/ryan/reactspringboot/ReactSpringBootApplication.java
@@ -1,13 +1,18 @@
 package com.ryan.reactspringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactSpringBootApplication {
 
+  private static final Logger logger = LoggerFactory.getLogger(ReactSpringBootApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReactSpringBootApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
 
 }
