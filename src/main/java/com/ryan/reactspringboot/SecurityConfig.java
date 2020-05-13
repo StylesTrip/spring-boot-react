828c166da281048f57650ad37b9141a687f5ec4c
Ryan T <ryan.turinsky@gmail.com>
2020-05-12 22:54:15 -0400

    Adding spring security and creating a couple users for authentication

--- src/main/java/com/ryan/reactspringboot/SecurityConfig.java
+++ src/main/java/com/ryan/reactspringboot/SecurityConfig.java
@@ -1,34 +1,24 @@
 package com.ryan.reactspringboot;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.ryan.reactspringboot.service.UserDetailServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  private UserDetailServiceImpl userDetailsService;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+    .passwordEncoder(new BCryptPasswordEncoder());
   }
-
-  @Bean
-  @Override
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withDefaultPasswordEncoder()
-    .username("user")
-    .password("password")
-    .roles("USER")
-    .build();
-
-    return new InMemoryUserDetailsManager(user);
-  }
-
 }
