4e86eff49783edf249aadbda04b58998dc8731d2
Ryan T <ryan.turinsky@gmail.com>
2020-05-26 22:56:22 -0400

    Adding unit tests. Had to add getter and setter for id in Car class for the one test

--- src/test/java/com/ryan/reactspringboot/ReactSpringBootApplicationTests.java
+++ src/test/java/com/ryan/reactspringboot/ReactSpringBootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ryan.reactspringboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ryan.reactspringboot.web.CarController;;
 
 @SpringBootTest
 class ReactSpringBootApplicationTests {
+	@Autowired
+	private CarController controller;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(controller);
 	}
 
 }
