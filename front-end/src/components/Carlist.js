6d4e1ccdcc68908fb5bc478456157ab7ef32c104
Ryan T <ryan.turinsky@gmail.com>
2020-07-23 23:03:20 -0400

    Adding delete car functionality with confirmation and toaster notify

--- front-end/src/components/Carlist.js
+++ front-end/src/components/Carlist.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import {SERVER_URL} from '../constants.js';
 import Table from "./Table";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Carlist = () => {
 
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
+    fetchCars();
+  }, []);
+
+  const fetchCars = () => {
     fetch(SERVER_URL + 'api/cars')
       .then((response) => response.json())
       .then((responseData) => {
@@ -14,7 +20,25 @@ const Carlist = () => {
           console.log(responseData._embedded.cars[0].color)
       })
       .catch(err => console.error(err));
-  }, []);
+  }
+
+  const onDelClick = (link) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      fetch(link, {method: 'DELETE'})
+        .then(res => {
+          toast.success("Car deleted", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          fetchCars();
+        })
+        .catch(err => {
+          toast.error("Error when deleting", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err);
+        });
+    }
+  }
 
   const columns = React.useMemo(
       () => [
@@ -33,6 +57,13 @@ const Carlist = () => {
       }, {
         Header: 'Price',
         accessor: 'price'
+      }, {
+        id: 'delbutton',
+        sortable: false,
+        filterable: false,
+        width: 100,
+        accessor: '_links.self.href',
+        Cell: ({value}) => (<button onClick={() => {onDelClick(value)}}>Delete</button>)
       }
       ],
       []
@@ -41,6 +72,7 @@ const Carlist = () => {
     return (
       <div className="App">
         <Table columns={columns} data={cars} />
+        <ToastContainer autoClose={1500} />
       </div>
     );
 }
